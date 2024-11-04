dc5c5289c78b311cd6419b77249ef4e419877389
akshay <akshaykulangara73@gmail.com>
2024-11-04 12:52:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+// function isLeapYear(year) {
+//     return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+// }
+
+
+// const year = parseInt(prompt("Enter a year:"));
+// if (isLeapYear(year)) {
+//     console.log(`${year} is a leap year.`);
+// } else {
+//     console.log(`${year} is not a leap year.`);
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////?
+
+// function isLeapYear(year) {
+//     if (year % 400 === 0) {
+//       return true;
+//     } else if (year % 100 === 0) {
+//       return false;
+//     } else if (year % 4 === 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+  
+  
+//   let year = 2024;
+//   console.log(isLeapYear(year));
+  
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// function calculatediscount(purchase) {
+//   let discount;
+
+//   if (purchase >=1000) {
+//     discount = purchase * 0.2; 
+//   } else if (purchase >= 500) {
+//     discount = purchase * 0.1; 
+//   } else {
+//     discount = purchase * 0.5; 
+//   }
+
+//   return discount;
+// }
+
+
+// let purchase =750;
+// let discount = calculatediscount(purchase);
+// console.log(`The discount is: ₹${discount}`); 
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// function calculateTicketPrice(age) {
+//   let ticketPrice;
+
+//   if (age < 12) {
+//     ticketPrice = 5; 
+//   } else if (age >= 12 && age < 60) {
+//     ticketPrice = 10; 
+//   } else {
+//     ticketPrice = 7; 
+//   }
+
+//   return ticketPrice;
+// }
+
+
+// let age = 25;
+// let price = calculateTicketPrice(age);
+// console.log(`The ticket price is: ₹${price}`); 
+
+
+
